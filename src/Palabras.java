fadd75b4cdde51826514796fed110815be2a657b
maria-sancho <alusan7934@ieselcaminas.org>
2023-02-02 14:28:07 +0100

    OrdenaPalabras

--- src/Palabras.java
+++ src/Palabras.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class Palabras {
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.List;
+import java.util.Comparator;
+
+
+public class Palabras {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Añade una frase: ");
+        String frase = scanner.nextLine();
+
+        List<String> palabras = Arrays.asList(frase.split(" "));
+        palabras.sort(Comparator.comparing(String::toLowerCase));
+
+        System.out.println("Palabras ordenadas: ");
+        for (String palabra : palabras) {
+            System.out.println(palabra);
+        }
+    }
 }
